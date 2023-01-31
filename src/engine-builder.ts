697aca0df8f624ce15a9bf5993e6103d5db1997b
pp_pro <chinese1236@qq.com>
2023-01-31 18:00:56 +0800

    init base logic

--- src/engine-builder.ts
+++ src/engine-builder.ts
@@ -1,26 +1,44 @@
+import * as fs from 'fs-extra';
+import * as ps from 'path';
 import { IFlagConfig, PlatformType } from "./config-parser";
 
 export interface IBuildOptions {
+    root: string;
     entries: string[];
     platform: PlatformType;
     flagConfig: Partial<IFlagConfig>;
+    resolveExtensions: string[];
 }
 
 export interface IBuildResult {
     [fileName: string]: {
         code: string;
-        map: string;
+        map?: string;
     }
 }
 
 export class EngineBuilder {
+    private _resolveExtensions = ['.ts'];
+
     constructor () {
 
     }
 
     build (options: IBuildOptions): Promise<IBuildResult> {
         return new Promise(resolve => {
-            resolve({})
+            const { root, resolveExtensions, } = options;
+            this._resolveExtensions = resolveExtensions ?? this._resolveExtensions;
+            const result: IBuildResult = {};
+            options.entries.forEach(item => {
+                item = ps.join(options.root, item).replace(/\\/g, '/');
+                const code = fs.readFileSync(item, 'utf-8');
+                // TODO: ast traverse to parse dependency
+                result[item] = {
+                    code
+                };
+            });
+            
+            resolve(result)
         });
     }
 }
--- test/engine-builder.test.ts
+++ test/engine-builder.test.ts
@@ -1,8 +1,10 @@
 import * as ccbuild from '../src/index'
+import * as ps from 'path';
 
 test('test base', async () => {
     const engineBuilder = new ccbuild.EngineBuilder();
     const buildResult = await engineBuilder.build({
+        root: __dirname,
         entries: [
             './test-source/exports/audio.ts',
             './test-source/exports/animation.ts'
