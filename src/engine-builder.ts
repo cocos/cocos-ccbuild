96b0ba6cf3b80e6a37a9fb8e759ffe28bc05903c
pp_pro <chinese1236@qq.com>
2023-02-01 14:33:21 +0800

    basic traverse

--- src/engine-builder.ts
+++ src/engine-builder.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs-extra';
 import * as ps from 'path';
+import * as parser from '@babel/parser';
+import traverse from '@babel/traverse';
 import { IFlagConfig, PlatformType } from "./config-parser";
 
 export interface IBuildOptions {
@@ -7,7 +9,8 @@ export interface IBuildOptions {
     entries: string[];
     platform: PlatformType;
     flagConfig: Partial<IFlagConfig>;
-    resolveExtensions: string[];
+    resolveExtensions?: string[];
+    outDir?: string;
 }
 
 export interface IBuildResult {
@@ -15,6 +18,12 @@ export interface IBuildResult {
         code: string;
         map?: string;
     }
+}
+
+export interface ICompileResult {
+    code: string;
+    map?: string;
+    deps: string[];
 }
 
 export class EngineBuilder {
@@ -26,19 +35,71 @@ export class EngineBuilder {
 
     build (options: IBuildOptions): Promise<IBuildResult> {
         return new Promise(resolve => {
-            const { root, resolveExtensions, } = options;
+            let { root, resolveExtensions, outDir } = options;
             this._resolveExtensions = resolveExtensions ?? this._resolveExtensions;
             const result: IBuildResult = {};
-            options.entries.forEach(item => {
-                item = ps.join(options.root, item).replace(/\\/g, '/');
-                const code = fs.readFileSync(item, 'utf-8');
-                // TODO: ast traverse to parse dependency
-                result[item] = {
-                    code
+            const compileRecursively = (file: string) => {
+                const compileResult = this._compileFile(file);
+                result[file] = {
+                    code: compileResult.code,
                 };
+                compileResult.deps.forEach(dep => {
+                    compileRecursively(dep);
+                });
+            };
+            options.entries.forEach(file => {
+                compileRecursively(file);
             });
+
+            if (outDir) {
+                for (const file in result) {
+                    const { code } = result[file];
+                    const targetFile = ps.join(outDir, ps.relative(root, file)).replace(/\\/g, '/');
+                    fs.outputFileSync(targetFile, code, 'utf8');
+                }
+            }
             
-            resolve(result)
+            resolve(result);
         });
+    }
+
+    _compileFile (file: string): ICompileResult {
+        const code = fs.readFileSync(file, 'utf-8');
+        const ast = parser.parse(code, {
+            sourceType: 'module'
+        });
+        let deps: string[] = [];
+        traverse(ast, {
+            Import (path) {
+                // @ts-ignore
+                deps.push(path.node.source.value)
+            },
+            ExportDeclaration (path) {
+                // @ts-ignore
+                const source = path.node.source
+                if (source) {
+                    deps.push(source.value)
+                }
+            },
+        });
+        deps = deps.map(dep => ps.join(ps.dirname(file), dep).replace(/\\/g, '/'));
+        const resolvedDeps: string[] = [];
+        deps.forEach(dep => {
+            for (let ext of this._resolveExtensions) {
+                const fileExt = dep + ext;
+                const indexExt = ps.join(dep, 'index').replace(/\\/g, '/') + ext; 
+                if (fs.existsSync(fileExt)) {
+                    resolvedDeps.push(fileExt);
+                } else if (fs.existsSync(indexExt)) {
+                    resolvedDeps.push(indexExt);
+                }
+            }
+        });
+
+        // TODO: compile code
+        return {
+            code,
+            deps: resolvedDeps,
+        };
     }
 }
--- test/engine-builder.test.ts
+++ test/engine-builder.test.ts
@@ -3,16 +3,50 @@ import * as ps from 'path';
 
 test('test base', async () => {
     const engineBuilder = new ccbuild.EngineBuilder();
+    const root = ps.join(__dirname, './test-source').replace(/\\/g, '/');
     const buildResult = await engineBuilder.build({
-        root: __dirname,
+        root,
         entries: [
-            './test-source/exports/audio.ts',
-            './test-source/exports/animation.ts'
+          ps.join(__dirname, './test-source/exports/audio.ts').replace(/\\/g, '/'),
+          ps.join(__dirname, './test-source/exports/animation.ts').replace(/\\/g, '/'),
         ],
         platform: 'OPEN_HARMONY',
         flagConfig: {
             DEBUG: true,
         },
+        outDir: ps.join(__dirname, './lib').replace(/\\/g, '/'),
     });
-    expect(buildResult).toMatchInlineSnapshot(`{}`);
+    const files = Object.keys(buildResult);
+    for (let file of files) {
+      const data = buildResult[file];
+      delete buildResult[file];
+      const relativeFile = ps.relative(root, file).replace(/\\/g, '/');
+      buildResult[relativeFile] = data;
+    }
+    expect(buildResult).toMatchInlineSnapshot(`
+{
+  "animation/animation.ts": {
+    "code": "export class Animation {
+    play () {}
+}",
+  },
+  "animation/index.ts": {
+    "code": "export * from './animation'",
+  },
+  "audio/index.ts": {
+    "code": "export * from './player';",
+  },
+  "audio/player.ts": {
+    "code": "export class Player {
+    play () {}
+}",
+  },
+  "exports/animation.ts": {
+    "code": "export * from '../animation'",
+  },
+  "exports/audio.ts": {
+    "code": "export * from '../audio'",
+  },
+}
+`);
 });
