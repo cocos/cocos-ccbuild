2c817b0ba8ba13ab8ea8f33e767a0396cef6c3dc
pp_pro <chinese1236@qq.com>
2023-02-08 19:12:57 +0800

    update temp test

--- test.mjs
+++ test.mjs
@@ -3,12 +3,48 @@ import * as ps from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = ps.dirname(fileURLToPath(import.meta.url));
 
-const engineRoot = 'c:/Users/pp/Desktop/cocos-editor/resources/3d/engine';
+const engineRoot = 'c:/Users/pp/Desktop/cocos-editor-legacy/resources/3d/engine';
 
 const engineBuilder = new ccbuild.EngineBuilder();
 await engineBuilder.build({
     root: engineRoot,
-    features: ['audio', 'animation'],
+    features: [
+        "base",
+        "gfx-webgl",
+        "gfx-webgl2",
+        "3d",
+        "animation",
+        "skeletal-animation",
+        "2d",
+        "ui",
+        "particle",
+        "particle-2d",
+        "physics-framework",
+        // "physics-cannon",
+        // "physics-physx",
+        // "physics-ammo",
+        "physics-builtin",
+        // "physics-2d-framework",
+        // "physics-2d-box2d",
+        // "physics-2d-builtin",
+        "intersection-2d",
+        "primitive",
+        "profiler",
+        // "occlusion-query",
+        // "geometry-renderer",
+        // "debug-renderer",
+        "audio",
+        "video",
+        // "xr",
+        // "terrain",
+        "webview",
+        "tween",
+        // "tiled-map",
+        // "spine",
+        // "dragon-bones",
+        // "marionette",
+        "custom-pipeline",
+    ],
     platform: 'NATIVE',
     mode: 'BUILD',
     flagConfig: {
