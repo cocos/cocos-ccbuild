f79409da956b761e23b6b9e98dbed04716408298
pp_pro <chinese1236@qq.com>
2023-03-07 20:39:20 +0800

    update test

--- test/test-source/audio/as-type.ts
+++ test/test-source/audio/as-type.ts
@@ -1,3 +1,5 @@
 const a = 1;
 const b = 2;
-const c = ((a as unknown as number) & b) as unknown as number; // this is a bug in babel
+// this is a bug in babel
+const c = ((a as unknown as number) & b) as unknown as number;
+const d = (a & b) as unknown as number;
