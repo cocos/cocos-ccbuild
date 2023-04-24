978b598d0430c0810f1200100d085becc193fe73
pp_pro <chinese1236@qq.com>
2023-04-24 21:28:08 +0800

    [WIP] wasm test case

---
+++ test/build-engine/engine-js.test.ts
@@ -0,0 +1,13 @@
+import { buildJsEngine } from '../../src/build-engine/engine-js';
+import * as ps from 'path';
+import { StatsQuery } from '../../src/stats-query';
+
+test('engine-js', async () => {
+    await buildJsEngine({
+        engine: ps.join(__dirname, '../test-engine-source'),
+        out: ps.join(__dirname, './lib-js'),
+        mode: 'BUILD',
+        platform: 'HTML5',
+        features: ['wasm-test']
+    });
+});
