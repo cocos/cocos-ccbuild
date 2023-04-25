1c69b82de272805f40457b65da082d665412a551
pp_pro <chinese1236@qq.com>
2023-04-25 17:28:19 +0800

    update test

--- test/build-engine/engine-js.test.ts
+++ test/build-engine/engine-js.test.ts
@@ -1,15 +1,54 @@
 import { buildJsEngine } from '../../src/build-engine/engine-js';
 import * as ps from 'path';
-import { StatsQuery } from '../../src/stats-query';
+import * as fs from 'fs-extra';
+import del from 'del';
 
 jest.setTimeout(10000);
-test('engine-js', async () => {
-    await buildJsEngine({
-        engine: ps.join(__dirname, '../test-engine-source'),
-        out: ps.join(__dirname, './lib-js'),
-        mode: 'BUILD',
-        platform: 'HTML5',
-        features: ['wasm-test'],
-        moduleFormat: 'system',
+
+async function readdirR (item: string, reduceOutput: string[]) { 
+    if ((await fs.stat(item)).isDirectory()) {
+        const dirItems = await fs.readdir(item);
+        for (let subItem of dirItems) {
+            await readdirR(ps.join(item, subItem), reduceOutput);
+        }
+    } else {
+        reduceOutput.push(item);
+    }
+}
+
+describe('engine-js', () => {
+    test('build WASM module on platform supporting WASM', async () => {
+        const out = ps.join(__dirname, './lib-js');
+        await buildJsEngine({
+            engine: ps.join(__dirname, '../test-engine-source'),
+            out,
+            mode: 'BUILD',
+            platform: 'HTML5',
+            features: ['wasm-test'],
+            moduleFormat: 'system',
+        });
+        let outputScripts: string[] = [];
+        await readdirR(out, outputScripts);
+        outputScripts = outputScripts.map(item => ps.relative(out, item));
+        expect(outputScripts).toMatchSnapshot();
+        await del(out, { force: true });
+    });
+
+    
+    test('build WASM module on platform not supporting WASM', async () => {
+        const out = ps.join(__dirname, './lib-js');
+        await buildJsEngine({
+            engine: ps.join(__dirname, '../test-engine-source'),
+            out,
+            mode: 'BUILD',
+            platform: 'XIAOMI',
+            features: ['wasm-test'],
+            moduleFormat: 'system',
+        });
+        let outputScripts: string[] = [];
+        await readdirR(out, outputScripts);
+        outputScripts = outputScripts.map(item => ps.relative(out, item));
+        expect(outputScripts).toMatchSnapshot();
+        await del(out, { force: true });
     });
 });
