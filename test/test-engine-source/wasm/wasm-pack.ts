a4b35be1e8c883cb1ee4cd7e439eeae3b9488786
pp_pro <chinese1236@qq.com>
2023-05-04 19:26:58 +0800

    optimize

--- test/test-engine-source/wasm/wasm-pack.ts
+++ test/test-engine-source/wasm/wasm-pack.ts
@@ -1,6 +1,6 @@
 import { WASM_SUPPORT_MODE } from 'internal:constants';
-import wasmUrl from 'external://wasm/wasm-pack/wasm_rust.wasm';
-import init from 'external:///wasm/wasm-pack/wasm_rust.js'
+import wasmUrl from 'external:wasm/wasm-pack/wasm_rust.wasm';
+import init from 'external:wasm/wasm-pack/wasm_rust.js'
 import { isSupportWASM } from './is-support-wasm';
 
 if (WASM_SUPPORT_MODE === 1) {
