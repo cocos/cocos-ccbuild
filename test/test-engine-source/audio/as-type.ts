d1792f1bb18410b577cdfce668823d45c5e8473c
PP_Pro <chinese1236@qq.com>
2023-07-24 12:46:47 +0800

    remove TODO test

--- test/test-engine-source/audio/as-type.ts
+++ test/test-engine-source/audio/as-type.ts
@@ -1,5 +1,5 @@
 const a = 1;
 const b = 2;
-// this is a bug in babel
-const c = ((a as unknown as number) & b) as unknown as number;
-const d = (a & b) as unknown as number;
+// TODO: this is a bug in babel
+// const c = ((a as unknown as number) & b) as unknown as number;
+// const d = (a & b) as unknown as number;
