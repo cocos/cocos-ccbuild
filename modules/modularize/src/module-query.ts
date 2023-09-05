7bb3ea22b64ea7a3c31de5f4c984f184d9965960
PP_Pro <chinese1236@qq.com>
2023-09-05 13:49:01 +0800

    fix: getAllModules in ModuleQuery

--- modules/modularize/src/module-query.ts
+++ modules/modularize/src/module-query.ts
@@ -56,7 +56,17 @@ export class ModuleQuery {
                 }));
             }
         }
+        pkgFiles = pkgFiles.map(file => {
+            if (fs.statSync(file).isDirectory()) {
+                const pkgFile = ps.join(file, 'package.json');
+                if (fs.existsSync(pkgFile)) {
+                    return pkgFile;
+                }
+            }
+            return file;
+        });
         pkgFiles = pkgFiles.filter(file => file.endsWith('package.json'));
+        pkgFiles = pkgFiles.filter((file, index) => pkgFiles.indexOf(file) === index);
         const moduleNames: string[] = [];
         for (const pkg of pkgFiles) {
             const name = (await fs.readJson(pkg)).name;
