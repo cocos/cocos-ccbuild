a16fef0a8fe3a7d0d2bd621b91e7e83ce4978e91
PP <chinese1236@qq.com>
2023-09-08 23:51:12 +0800

    Update test/test-engine-source-without-symlink/@types/legacy-compatibility.d.ts

--- test/test-engine-source-without-symlink/@types/legacy-compatibility.d.ts
+++ test/test-engine-source-without-symlink/@types/legacy-compatibility.d.ts
@@ -1,4 +1,4 @@
-// we have no defined @types/cc-ambient-types module in this engine project
+// we have no defined '@types/cc-ambient-types' module in this engine project
 // so we fallback to the legacy resolve to keep the compatibility.
 // we need to ensure that the engine build tools could still work 
 // when engine has no @types/cc-ambient-types module.
