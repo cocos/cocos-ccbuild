ff7f101516ab09836466b643abeeb5c27aa974d9
PP_Pro <chinese1236@qq.com>
2023-09-09 10:57:58 +0800

    remove chalk

--- .github/workflows/scripts/api-check.js
+++ .github/workflows/scripts/api-check.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const ps = require('path');
-const chalk = require('chalk');
 
 const oldVer = ps.join(__dirname, '../../../.api/public_old.d.ts');
 const newVer = ps.join(__dirname, '../../../.api/public.d.ts');
@@ -8,6 +7,6 @@ const oldContent = fs.readFileSync(oldVer, 'utf8');
 const newContent = fs.readFileSync(newVer, 'utf8');
 
 if (oldContent !== newContent) {
-    console.error(chalk.red(`please run 'npm run api' to update public.d.ts, and commit this file.`));
+    console.error(`please run 'npm run api' to update public.d.ts, and commit this file.`);
     process.exit(1);
 }
