6030c15cf895217b851b4785194691164f9c6af7
James Chen <dumganhar@gmail.com>
2024-12-24 17:44:58 +0800

    Update test

--- scripts/test-build-cocos.js
+++ scripts/test-build-cocos.js
@@ -25,7 +25,7 @@ const argv = process.argv;
         "out": outDir,
         "platform": "WECHAT",
         "moduleFormat": "system",
-        "compress": true,
+        "compress": false,
         "split": false,
         "nativeCodeBundleMode": "wasm",
         "assetURLFormat": "relative-from-out",
@@ -34,19 +34,21 @@ const argv = process.argv;
         // "features": ["2d", "3d", "animation", "audio", "base", "debug-renderer", "dragon-bones", "geometry-renderer", "gfx-webgl", "intersection-2d", "legacy-pipeline", "light-probe", "particle", "particle-2d", "physics-2d-builtin", "physics-cannon", "primitive", "profiler", "skeletal-animation", "spine", "terrain", "tiled-map", "tween", "ui", "video", "websocket", "webview"],
         // "features":["gfx-webgl2"],
         "features": ["2d","audio","base","gfx-webgl2","legacy-pipeline","ui"], //,"animation","spine","tween"
+        // "features": ["mangle-private-properties-test"],
         // "features":["base", "audio", "2d", "ui", "gfx-webgl2"],
         "loose": true,
         "mode": "BUILD",
         "flags": {
             "DEBUG": false,
-            ONLY_2D: true,
+            // ONLY_2D: true,
             "WEBGPU": false
         },
         // "metaFile": ps.join(outDir, "meta.json"),
         // "incremental": ps.join(outDir, "watch-files.json"),
         "wasmCompressionMode": 'brotli',
-        "visualize": true,
-        "inlineEnum": true,
+        // "visualize": true,
+        // "inlineEnum": true,
+        mangleProperties: true,
     };
 
     await ensureDir(outDir);
