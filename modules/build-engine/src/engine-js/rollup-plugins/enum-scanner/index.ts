c38d0eefe5f25c475d183a8c692203a532a283da
James Chen <dumganhar@gmail.com>
2024-12-26 21:15:38 +0800

    Update @cocos/typescript version.

--- modules/build-engine/src/engine-js/rollup-plugins/enum-scanner/index.ts
+++ modules/build-engine/src/engine-js/rollup-plugins/enum-scanner/index.ts
@@ -16,6 +16,17 @@ export async function rpEnumScanner(rawOptions: Options): Promise<rollup.Plugin[
     const options = resolveOptions(rawOptions);
 
     enumData = await scanEnums(options);
+    const { defines } = enumData;
+
+    defines['Float32Array.BYTES_PER_ELEMENT'] =  4;
+    defines['Float64Array.BYTES_PER_ELEMENT'] =  8;
+    defines['Uint8Array.BYTES_PER_ELEMENT'] =  1;
+    defines['Uint8ClampedArray.BYTES_PER_ELEMENT'] = 1;
+    defines['Uint16Array.BYTES_PER_ELEMENT'] =  2;
+    defines['Uint32Array.BYTES_PER_ELEMENT'] =  4;
+    defines['Int8Array.BYTES_PER_ELEMENT'] =  1;
+    defines['Int16Array.BYTES_PER_ELEMENT'] =  2;
+    defines['Int32Array.BYTES_PER_ELEMENT'] =  4;
 
     return [];
 }
