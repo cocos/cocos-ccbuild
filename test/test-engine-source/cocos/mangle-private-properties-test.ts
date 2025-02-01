7c714fee1c73459e0a848b312a1e84a223118d95
James Chen <dumganhar@gmail.com>
2025-02-01 15:38:07 +0800

    Update

--- test/test-engine-source/cocos/mangle-private-properties-test.ts
+++ test/test-engine-source/cocos/mangle-private-properties-test.ts
@@ -926,8 +926,8 @@ mangleQuestionProperties.test();
 
 /** @mangle */
 export class MangleTestMangleWholeClassIncludingStatic {
-    // public _myProp1: number = 123;
-    // public getMyProp1(): number { return this._myProp1; }
+    public _myProp1: number = 123;
+    public getMyProp1(): number { return this._myProp1; }
     public static _timers = [];
     public static getTimers(): any[] { return MangleTestMangleWholeClassIncludingStatic._timers; }
 }
